fd329a86823de00e5fa54ed49a776bf0dc6107b3
alkosmas92 <alkosmas.92@gmail.com>
2021-10-20 14:29:13 +0300

    Change Singup

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.post("/failure", function(req,res){
     res.redirect("/");
 })
 
-app.listen(process.env.PORT , function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
 
